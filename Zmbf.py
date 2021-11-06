e9349e53ac333803fea9a064381f7b7c83db9a91
ZehanPurnamma1 <93824613+ZehanPurnamma1@users.noreply.github.com>
2021-11-06 23:10:44 +0700

    Update Zmbf.py

--- Zmbf.py
+++ Zmbf.py
@@ -52,11 +52,11 @@ _H_ = "\x1b[0;92m" # Hijau
 _U_ = "\x1b[0;95m" # Ungu
 
 ### Logo
-_logo_line_1_ = ('%s___________________Hans___________________'%(_P_))
-_logo_line_2_ = ('%s| %s┌─────────────────────────┐'%(_P_,_U_))
-_logo_line_3_ = ('%s| %s│   %s• Code By Zehan •   %s│'%(_P_,_U_,_P_,_U_))
-_logo_line_4_ = ('%s| %sGithub.com/ZehanPurnamma1/Zmbf %s│'%(_P_,_U_,_P_,_U_))
-_logo_line_5_ = ('%s XNSCODE Team 2021    └─────────────────────────┘'%(_U_))
+_logo_line_1_ = ('%s ___  __  __ ___ ___ '%(_P_))
+_logo_line_2_ = ('%s|   \|  \/  | _ ) __| %s┌─────────────────────────┐'%(_P_,_U_))
+_logo_line_3_ = ('%s| |) | |\/| | _ \ _|  %s│   %s• Code By Zhan •   %s│'%(_P_,_U_,_P_,_U_))
+_logo_line_4_ = ('%s|___/|_|  |_|___/_|   %s│ %sGithub.com/ZehanPurnamma1/Zmbf %s│'%(_P_,_U_,_P_,_U_))
+_logo_line_5_ = ('%s XNSCODE Team 2021    └─────────────────────────┘'%(_U_))──┘'%(_U_))
 def _my_logo_():
     _dapunta_cici_(_logo_line_1_)
     _dapunta_cici_(_logo_line_2_)
